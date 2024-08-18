10e965b020bbc0477d4ecd393a74f9034886cd10
Chamodh Jayasumana <chamodhinduwarajayasumana@gmail.com>
2024-08-19 00:24:02 +0530

    finish all styls

--- xygen/src/components/NavBar/navbar.js
+++ xygen/src/components/NavBar/navbar.js
@@ -7,14 +7,18 @@ import {Link} from 'react-scroll'
 const Navbar = () => {
   return (
     <nav className="Navbar">
-      <img src={logo} alt="logo" className='logo' />
+      <img src={logo} alt="logo" className='logo' onClick={()=>{
+        document.getElementById('intro').scrollIntoView({behavior:'smooth'});
+      }}/>
       <div className="desktopMenu">
-          <Link className="descktopMenuListItem">Home</Link>
-          <Link className="descktopMenuListItem">About Us</Link>
-          <Link className="descktopMenuListItem">Services</Link>
-          <Link className="descktopMenuListItem">Client</Link>
+          <Link activeClass='active' to='intro' spy={true} offset={-100} duration={500} className="descktopMenuListItem">Home</Link>
+          <Link activeClass='active' to='feature' spy={true} offset={-100} duration={500}  className="descktopMenuListItem">About Us</Link>
+          <Link activeClass='active' to='works' spy={true} offset={-100} duration={500}  className="descktopMenuListItem">Services</Link>
+          <Link activeClass='active' to='contactPage' spy={true} offset={-100} duration={500}  className="descktopMenuListItem">Client</Link>
       </div>
-      <button className="descktopMenuBtn">
+      <button className="descktopMenuBtn" onClick={()=>{
+        document.getElementById('contact').scrollIntoView({behavior:'smooth'});
+      }}>
           <img src={contactImg} art="" className="descktopMenuImg"/>Contact Us</button>
     </nav>
   )
